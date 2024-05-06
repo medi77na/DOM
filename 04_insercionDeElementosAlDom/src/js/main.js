6f8e0c7ae22eeef58efdaabc106ba30cea9ab26c
medi77na <medinastevenurregodavid@gmail.com>
2024-05-06 06:43:55 -0500

    Se comienza a trabajar en el documento con ejemplos

---
+++ 04_insercionDeElementosAlDom/src/js/main.js
@@ -0,0 +1,24 @@
+function appendChildExample() {
+
+    const btnExecute = document.getElementById('btn-execute');
+
+    //Función anónima (tipo Flecha)
+    btnExecute.addEventListener('click', () => {
+        alert("Hola mundo!");
+    });
+
+}
+
+function removeChildExample() {
+
+}
+
+function replaceChildExample() {
+
+}
+
+function insertBeforeExample() {
+
+}
+
+appendChildExample()
